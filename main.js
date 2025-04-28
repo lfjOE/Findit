c7a084c051966e403b793d46ef6a12ed65a447bc
LuisOsorioE <luisfelipeosorioe@gmail.com>
2025-04-27 20:50:23 -0600

    Se añaden validaciones y se pone como invisibles los resultados por defecto
    Se valida que haya texto en el input y que hayan tiendas seleccionadas, de lo contrario, el botón de búsqueda queda deshabilitado, además, al buscar, nos baja directo a la sección de resultados

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 const storesImages = document.querySelectorAll('.stores');
+
+const findButton = document.querySelector('#find-button');
+findButton.disabled = true;
+
+const results = document.querySelector('#results');
+
+const searchInput = document.querySelector('#search');
+
+console.log(findButton.innerHTML);
 const selectedStores = [];
+
+function verifyInput(){
+    if (searchInput.value.trim() !== "" && selectedStores.length > 0) {
+        findButton.disabled = false; 
+    } else {
+        findButton.disabled = true; 
+    }
+}
+
+function scrollToResults() {
+    const resultsSection = document.querySelector('#results');
+    resultsSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'     
+    });
+}
 
 storesImages.forEach(image=>{
     image.parentElement.addEventListener('click', ()=>{
@@ -12,7 +37,23 @@ storesImages.forEach(image=>{
             selectedStores.splice(index,1);
         }else{
             image.parentElement.classList.add('selected');
+            selectedStores.push(storeName);
         }
         console.log(selectedStores);
+        verifyInput();
     })
 })
+
+findButton.addEventListener('click', ()=>{
+    findProducts();
+    scrollToResults();
+});
+
+searchInput.addEventListener('input', () => {
+    verifyInput();
+});
+
+
+function findProducts(){
+    results.classList.remove('d-none');
+}
